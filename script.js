acb898eb54c948249d97afe1d6bf2cc88045a5de
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-05-19 13:22:28 -0400

    my initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const body = document.querySelector('body');
+body.style.display = 'flex';
+body.style.justifyContent = 'center';
+const mainDiv1 = document.createElement('div');//main div for form
+body.append(mainDiv1);
+mainDiv1.style.backgroundColor = 'skyblue';
+mainDiv1.style.display = 'flex';
+mainDiv1.style.justifyContent = 'center';
+mainDiv1.style.width = '100%'
+mainDiv1.style.border = ' 1rem solid midnightblue'
+//
+const formDiv = document.createElement('div');//div the form is in (formDiv)
+formDiv.style.height = '20rem';
+formDiv.style.width = '20rem';
+formDiv.style.backgroundColor = 'lightgray';
+formDiv.style.borderRadius = '2rem';
+formDiv.style.margin = '2rem';
+
+formDiv.setAttribute('id', 'formDiv');
+mainDiv1.appendChild(formDiv);
+const myform = document.createElement('form');//the form lable
+formDiv.appendChild(myform);
+let label1 = document.createElement('label');//
+let inputFN1 = document.createElement('input');
+let inputLN1 = document.createElement('input');
+let inputSubmit = document.createElement('input')
+//append to formDiv
+label1.textContent = 'Name';
+inputFN1.setAttribute('type', 'text');
+inputLN1.setAttribute('type', 'text');
+inputSubmit.setAttribute('type', 'sumbit');
