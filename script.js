54ef4b574ebd408cd17a5c183b4943d27abfb3b4
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-05-19 14:15:31 -0400

    made complex changes that is not working so about to add to html file for next commit

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ mainDiv1.style.width = '100%'
 mainDiv1.style.border = ' 1rem solid midnightblue'
 //
 const formDiv = document.createElement('div');//div the form is in (formDiv)
+formDiv.style.display = 'flex'
+//formDiv.style.flexDirection = 'column'
+//formDiv.style.justifyContent = 'space-around'
 formDiv.style.height = '20rem';
 formDiv.style.width = '20rem';
 formDiv.style.backgroundColor = 'lightgray';
@@ -20,12 +23,26 @@ formDiv.setAttribute('id', 'formDiv');
 mainDiv1.appendChild(formDiv);
 const myform = document.createElement('form');//the form lable
 formDiv.appendChild(myform);
-let label1 = document.createElement('label');//
-let inputFN1 = document.createElement('input');
-let inputLN1 = document.createElement('input');
-let inputSubmit = document.createElement('input')
+
+
+let label1 = document.createElement('label');//lable for the form
+let break1 = document.createElement('br');
+let inputFN1 = document.createElement('input');//first name input (inputFN1)
+let break2 = document.createElement('br');
+let inputLN1 = document.createElement('input');//last name input (inputLN1)
+let inputDate =document.createElement('input');// date input for date of birth constraint (inputDate)
+let inputSubmit = document.createElement('input');// my sumbit button
+inputSubmit.setAttribute('type', 'button');
+
 //append to formDiv
 label1.textContent = 'Name';
 inputFN1.setAttribute('type', 'text');
 inputLN1.setAttribute('type', 'text');
 inputSubmit.setAttribute('type', 'sumbit');
+inputDate.setAttribute('type', 'date');
+//myform.appendChild(label1);
+myform.appendChild(inputFN1);
+myform.appendChild(inputLN1);
+myform.appendChild(inputDate);
+myform.appendChild(inputSubmit);
+myform.appendChild(break1);
