8d52ea24e82da12527e625937690b53fa15d8dd4
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-05-20 16:38:39 -0400

    going in another direction due to DOM failures and time constraints

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// const body = document.querySelector('body');
+ const body = document.querySelector('body');
+ body.style.backgroundColor = 'grey'
 // body.style.display = 'flex';
 // body.style.justifyContent = 'center';
 // const mainDiv1 = document.createElement('div');//main div for form
@@ -46,3 +47,39 @@
 // myform.appendChild(inputDate);
 // myform.appendChild(inputSubmit);
 // myform.appendChild(break1);
+//--ABOVE IS DOM MINIPULATED CODE THAT FAILS TO DO WHAT I NEED IT TO DO----
+
+
+let submit = document.getElementById('submit');
+let mainDiv = document.getElementById('mainDiv');
+let Fname = document.getElementById('Fname');
+
+// let inputFeild = document.getElementById('Fname').value;
+// // let filled = inputFeild.defaultValue = '';
+
+// let inputFeild1 = document.getElementById("Lname");
+// // inputFeild1.defaultValue = '';
+let gameDiv = document.createElement('div');
+gameDiv.setAttribute('id', 'gameBoard');
+body.append(gameDiv);
+const gameBoard = document.querySelector('gameBoard')
+ 
+
+submit.addEventListener('click', e =>{
+    //  e.target.preventDefalt();
+
+    // if(inputFeild === ""){
+    //     mainDiv.style.backgroundColor ='red';
+    // }else if(inputFeild === true){
+    //     mainDiv.style.backgroundColor ='rgb(98, 237, 98)';  
+    // }//--create game board--
+  
+
+    
+setTimeout(function(){
+    mainDiv.style.backgroundColor ='skyblue'}, 3000);
+})
+
+
+let rule = document.createElement('hr')
+body.append(rule)
