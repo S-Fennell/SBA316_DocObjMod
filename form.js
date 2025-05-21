41af51ab5caf3597d78ca12565d4eae77d8317f3
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-05-21 18:03:03 -0400

    simple validation. trying to add error message

--- form.js
+++ form.js
@@ -1,43 +1,101 @@
 const body = document.querySelector('body');
 const formContainer = document.createElement('div');//div form container
-const form = document.createElement('form');
+const form = document.createElement('form');//<form>
 
 const label = document.createElement('label');
 const input = document.createElement('input');
+const span = document.createElement('span');
 
 const label1 = document.createElement('label');
 const input1 = document.createElement('input');
+const span1 = document.createElement('span');
 
 const label2 = document.createElement('label');
 const input2 = document.createElement('input');
+const span2 = document.createElement('span');
 
 const label3 = document.createElement('label');
 const input3 = document.createElement('input');
+const span3 = document.createElement('span');
+
+const subBtn = document.createElement('button');
+
+let username = document.getElementById('username');
+
 
 formContainer.appendChild(form);//placing <form> inside <div>=formContainer
 //placing <label> and <input> inside of <form>
-form.appendChild(label);
+form.appendChild(label).innerText= 'Username:';
+label.setAttribute('id', 'user');
 form.appendChild(input);
 
-form.appendChild(label1);
+
+form.appendChild(label1).innerText= 'Email:';
+label1.setAttribute('id', 'Email');
 form.appendChild(input1);
 
-form.appendChild(label2);
+form.appendChild(label2).innerText= 'Password:';
+label2.setAttribute('id', 'pw');
 form.appendChild(input2);
 
-form.appendChild(label3);
+form.appendChild(label3).innerText= 'Confrim Password:';
+label3.setAttribute('id', 'cpw');
 form.appendChild(input3);
 
+form.appendChild(subBtn).innerText= 'Submit';
+
+input.setAttribute('id', 'username');
 input.setAttribute('type', 'text');
 input.setAttribute('name', 'name');
 input.setAttribute('placeholder', 'username');
-// input.setAttribute('required');
+input.setAttribute('minlength', '6');
+input.setAttribute('maxlength', '9');
+input.setAttribute('required', '');
+
+input1.setAttribute('id', 'email')
+input1.setAttribute('type', 'email');
+input1.setAttribute('name', 'name');
+input1.setAttribute('placeholder', 'email');
+input1.setAttribute('minlength', '3');
+input1.setAttribute('maxlength', '12');
+input1.setAttribute('required', '');
+
+input2.setAttribute('id', 'password')
+input2.setAttribute('type', 'password');
+input2.setAttribute('name', 'name');
+input2.setAttribute('placeholder', 'password');
+input2.setAttribute('minlength', '7');
+input2.setAttribute('maxlength', '14');
+input2.setAttribute('required', '');
+
+input3.setAttribute('id', 'password1');
+input3.setAttribute('type', 'password1');
+input3.setAttribute('name', 'name');
+input3.setAttribute('placeholder', 'Re-enter password');
+input3.setAttribute('required', '');
+
+span.setAttribute('id', 'usernameError');
+//span1.setAttribute('id', 'nameError');
+span2.setAttribute('id', 'passwordError');
+span3.setAttribute('id', 'nameError');
 
 
-
-
-label.innerText= 'Username';
-label.setAttribute('id', 'user');
 formContainer.classList.add('container');
 body.appendChild(formContainer);
 body.style.backgroundColor = 'grey';
+
+subBtn.addEventListener('click',(e)=>{
+
+    //e.preventDefault()
+    const div = document.createElement('div');
+    div.style.backgroundColor = 'blue';
+    div.style.borderRadius = '50%';
+    div.style.height= '80px';
+    div.style.width = '80px';
+    body.appendChild(div);
+})
+
+form.addEventListener('change', ()=>{
+    let username = usernameInput.value;
+})
+
