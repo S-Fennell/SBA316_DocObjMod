80a6b1631461f2d9586fdf013b348f8b06a8a5c8
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-05-22 18:56:56 -0400

    starting a tic tac toe game

--- form.js
+++ form.js
@@ -59,8 +59,6 @@ input.setAttribute('minlength', '6');
 input.setAttribute('maxlength', '9');
 //input.setAttribute('required', '');
 
-
-
 input1.setAttribute('id', 'email')
 input1.setAttribute('type', 'email');
 input1.setAttribute('name', 'email');
@@ -92,7 +90,6 @@ formContainer.classList.add('container');
 body.appendChild(formContainer);
 body.style.backgroundColor = 'grey';
 
-
 //testing functionality:
 subBtn.addEventListener('mousemove',()=>{
     input.backgroundColor = 'blue';
@@ -101,7 +98,6 @@ subBtn.addEventListener('mousemove',()=>{
 
 subBtn.addEventListener('click',userValidation);
 
- 
 //span error message to be displayed but
 function userValidation(){
        const div = document.createElement('div');
@@ -113,7 +109,7 @@ function userValidation(){
     div.style.justifyContent = 'center';
     div.style.alignContent = 'center';
     body.appendChild(div);
-    div.innerHTML = '<h5>test dot!</h5>'
+    div.innerHTML = '<h5>test dot!</h5>';
 
     let userName = document.getElementById('username').value;
      if(userName.length === ""){
@@ -122,7 +118,6 @@ function userValidation(){
     span.innerText = 'valid';
 }
    
-  
 form.addEventListener('change', ()=>{
     const div = document.createElement('div');
     div.style.backgroundColor = 'blue';
@@ -133,14 +128,15 @@ form.addEventListener('change', ()=>{
     div.style.justifyContent = 'center';
     div.style.alignContent = 'center';
     body.appendChild(div);
-    div.innerHTML = '<h5>test dot!</h5>'
+    div.innerHTML = '<h5>test dot!</h5>';
 })
 const border = document.createElement('hr');
 //------new----
 
 const gameFrame = document.createElement('div');
 const gameContainer = document.createElement('div');
-const boxDiv0 = document.createElement('div')
+gameContainer.setAttribute('id', 'container');
+const boxDiv0 = document.createElement('div');
 const boxDiv1 = document.createElement('div');
 const boxDiv2 = document.createElement('div');
 const boxDiv3 = document.createElement('div');
@@ -149,6 +145,62 @@ const boxDiv5 = document.createElement('div');
 const boxDiv6 = document.createElement('div');
 const boxDiv7 = document.createElement('div');
 const boxDiv8 = document.createElement('div');
+
+gameContainer.style.border = '4px solid black';
+gameContainer.style.display = 'grid';
+gameContainer.style.gridTemplateColumns = 'repeat(3, 80px)';
+gameContainer.style.gap = '10px';
+gameContainer.style.margin = 'auto';
+gameContainer.style. width = 'max-content';
+gameContainer.style.padding = "20px";
+
+//squares
+boxDiv0.style.border= '3px solid black';
+boxDiv0.style.height= '80px';
+boxDiv0.style.width= '80px';
+
+boxDiv1.style.border= '3px solid black';
+boxDiv1.style.height= '80px';
+boxDiv1.style.width= '80px';
+
+boxDiv2.style.border= '3px solid black';
+boxDiv2.style.height= '80px';
+boxDiv2.style.width= '80px';
+
+boxDiv3.style.border= '3px solid black';
+boxDiv3.style.height= '80px';
+boxDiv3.style.width= '80px';
+
+boxDiv4.style.border= '3px solid black';
+boxDiv4.style.height= '80px';
+boxDiv4.style.width= '80px';
+
+boxDiv5.style.border= '3px solid black';
+boxDiv5.style.height= '80px';
+boxDiv5.style.width= '80px';
+
+boxDiv6.style.border= '3px solid black';
+boxDiv6.style.height= '80px';
+boxDiv6.style.width= '80px';
+
+boxDiv7.style.border= '3px solid black';
+boxDiv7.style.height= '80px';
+boxDiv7.style.width= '80px';
+
+boxDiv8.style.border= '3px solid black';
+boxDiv8.style.height= '80px';
+boxDiv8.style.width= '80px';
+
+boxDiv0.classList.add('boxes');
+boxDiv1.classList.add('boxes');
+boxDiv2.classList.add('boxes');
+boxDiv3.classList.add('boxes');
+boxDiv4.classList.add('boxes');
+boxDiv5.classList.add('boxes');
+boxDiv6.classList.add('boxes');
+boxDiv7.classList.add('boxes');
+boxDiv8.classList.add('boxes');
+
 gameContainer.appendChild(boxDiv0);
 gameContainer.appendChild(boxDiv1);
 gameContainer.appendChild(boxDiv2);
@@ -159,6 +211,12 @@ gameContainer.appendChild(boxDiv6);
 gameContainer.appendChild(boxDiv7);
 gameContainer.appendChild(boxDiv8);
 gameFrame.appendChild(gameContainer);
+
+//There seems to be nothing I can do to access this className to give them all a heigth and width. so how I have to do them all seprately.
+// const boxes = document.getElementById('container').getElementsByClassName('boxes');
+// boxes.style.height = '80px';
+// boxes.style.width = '80px';
+
 const gameTitle = document.createElement('h1')
 gameTitle.textContent = 'Tic Tac Toe';
 gameFrame.prepend(gameTitle);
@@ -168,8 +226,28 @@ restart_btn.innerText = 'RESTART';
 const winnerIs = document.createElement('h4');
 winnerIs.innerText = 'Winner is!';
 gameFrame.appendChild(winnerIs);
-gameFrame.appendChild(restart_btn)
+gameFrame.appendChild(restart_btn);
 boxDiv0.classList.add('boxes');
 
+const array = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7 ,8],
+    [0, 3, 6],
+    [1 ,4 ,7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let x = 'X';
+let o = 'O';
+const picks = ['', '', '', '', '', '', '', '', ''];
+const player = x;
+const gameOn = false;
 
+function startGame(){
+    boxes.forEach(boxes => boxes.addEventListener('click',boxPicked))
+    newGame.addEventListener('click', startNewGame);
+
+}
 
